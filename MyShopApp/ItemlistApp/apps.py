c726f4eed55010953b04449ac7f86ba87ec03d6b
Tomasz <core.editor>
2022-11-01 17:33:26 +0100

    replaced name and location of application

--- MyShopApp/ItemlistApp/apps.py
+++ MyShopApp/ItemlistApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ShopHomepageConfig(AppConfig):
+class ItemlistappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shop_homepage'
+    name = 'ItemlistApp'
