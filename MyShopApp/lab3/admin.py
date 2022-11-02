44bb53db697bc59c235e897bbd3bca966beff55b
Tomasz <core.editor>
2022-11-02 18:25:58 +0100

    fixed timestamps and admin view

--- MyShopApp/lab3/admin.py
+++ MyShopApp/lab3/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Osoba
 # Register your models here.
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko', 'miesiac_urodzenia', 'data_dodania']
 
-admin.site.register(Osoba)
+
+
+admin.site.register(Osoba,PersonAdmin)
--- MyShopApp/lab3/models.py
+++ MyShopApp/lab3/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import date
+from django.utils import timezone
 
 
 # Create your models here.
@@ -7,19 +7,34 @@ from datetime import date
 class Osoba(models.Model):
     imie = models.CharField(max_length=50)
     nazwisko = models.CharField(max_length=50)
-    wybor_miesiaca = (
-        ('1', 'Styczeń'),
-        ('2', 'Luty'),
-        ('3', 'Marzec'),
-        ('4', 'Kwiecień'),
-        ('5', 'Maj'),
-        ('6', 'Czerwiec'),
-        ('7', 'Lipiec'),
-        ('8', 'Sierpień'),
-        ('9', 'Wrzesień'),
-        ('10', 'Październik'),
-        ('11', 'Listopad'),
-        ('12', 'Grudzień')
-    )
-    miesiac_urodzenia = models.CharField(blank=True, choices=wybor_miesiaca, max_length=20, default=4)
-    data_dodania = models.DateField(default=date.today())
+
+    class WyborMiesiaca(models.IntegerChoices):
+        STYCZEN = 1
+        LUTY = 2
+        MARZEC = 3
+        KWIECIEN = 4
+        MAJ = 5
+        CZERWIEC = 6
+        LIPIEC = 7
+        SIERPIEN = 8
+        WRZESIEN = 9
+        PAZDZIERNIK = 10
+        LISTOPAD = 11
+        GRUDZIEN = 12
+
+    # wybor_miesiaca = (
+    #     ('1', 'Styczeń'),
+    #     ('2', 'Luty'),
+    #     ('3', 'Marzec'),
+    #     ('4', 'Kwiecień'),
+    #     ('5', 'Maj'),
+    #     ('6', 'Czerwiec'),
+    #     ('7', 'Lipiec'),
+    #     ('8', 'Sierpień'),
+    #     ('9', 'Wrzesień'),
+    #     ('10', 'Październik'),
+    #     ('11', 'Listopad'),
+    #     ('12', 'Grudzień')
+    # )
+    miesiac_urodzenia = models.IntegerField(choices=WyborMiesiaca.choices)
+    data_dodania = models.DateTimeField(auto_now_add=True)
