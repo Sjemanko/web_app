0fa4c0307fe123a01c6da563e58768f1ff9d104c
Tomasz <core.editor>
2022-11-02 16:02:22 +0100

    added models, check database with sqlite3, added debug tool

--- MyShopApp/ItemlistApp/models.py
+++ MyShopApp/ItemlistApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class ClothItem(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    clothItem = models.ForeignKey(ClothItem, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- MyShopApp/ItemlistApp/views.py
+++ MyShopApp/ItemlistApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from .models import ClothItem, User
+
 
 # Create your views here.
+
+
+def index(request, id):
+    item = ClothItem.objects.get(id=id)
+    return HttpResponse('<h1>%s</h1>' % item.name)
