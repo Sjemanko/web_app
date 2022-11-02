0ce5db6ae6e7da66d53d88279f00934d45c09b86
Tomasz <core.editor>
2022-11-02 17:05:39 +0100

    exercises 1, 2 from lab3

---
+++ MyShopApp/lab3/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Osoba(models.Model):
+    imie = models.CharField(max_length=50)
+    nazwisko = models.CharField(max_length=50)
+    wybor_miesiaca = (
+        ('1', 'Styczeń'),
+        ('2', 'Luty'),
+        ('3', 'Marzec'),
+        ('4', 'Kwiecień'),
+        ('5', 'Maj'),
+        ('6', 'Czerwiec'),
+        ('7', 'Lipiec'),
+        ('8', 'Sierpień'),
+        ('9', 'Wrzesień'),
+        ('10', 'Październik'),
+        ('11', 'Listopad'),
+        ('12', 'Grudzień')
+    )
+    miesiac_urodzenia = models.CharField(blank=True, choices=wybor_miesiaca, max_length=20, default=4)
