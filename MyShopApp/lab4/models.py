461ecf3d691cd1e141f01d121e7d5678db765925
Tomasz <core.editor>
2022-11-05 22:31:14 +0100

    added new models and serializers

--- MyShopApp/lab4/models.py
+++ MyShopApp/lab4/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f'{self.name} ({self.country})'
+
+
+class MonthBirth(models.IntegerChoices):
+    JANUARY = 1
+    FEBRUARY = 2
+    MARCH = 3
+    APRIL = 4
+    MAY = 5
+    JUNE = 6
+    JULY = 7
+    AUGUST = 8
+    SEPTEMBER = 9
+    OCTOBER = 10
+    NOVEMBER = 11
+    DECEMBER = 12
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    month_birth = models.IntegerField(choices=MonthBirth.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ["last_name"]
---
+++ MyShopApp/lab4/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Person, Team, MonthBirth
+
+
+class PersonSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=50, required=True)
+    last_name = serializers.CharField(max_length=50, required=True)
+    month_birth = serializers.ChoiceField(choices=MonthBirth)
+    team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all)
+    created_at = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Person.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.month_birth = validated_data.get('month_birth', instance.month_birth)
+        instance.team = validated_data.get('team', instance.team)
+        instance.created_at = validated_data.get('created_at', instance.created_at)
+        instance.save()
+        return instance
+
+
+class TeamSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100, required=True)
+    country = serializers.CharField(max_length=2, required=True)
+
+    def create(self, validated_data):
+        return Team.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.country = validated_data.get('country', instance.country)
+        instance.save()
+        return instance
