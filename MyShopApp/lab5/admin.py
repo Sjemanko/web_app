d2d76270b1b326f5cfc9ed585297cb3ebe268106
Tomasz <core.editor>
2022-11-06 20:50:37 +0100

    added lab5, set some validations

---
+++ MyShopApp/lab5/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Person, Team
+
+
+# Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'month_birth', 'created_at']
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country']
+
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Team, TeamAdmin)
---
+++ MyShopApp/lab5/serializers.py
@@ -0,0 +1,37 @@
+import datetime
+
+from rest_framework import serializers
+from .models import Person, Team, MonthBirth
+
+
+class PersonSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=50, required=True)
+    last_name = serializers.CharField(max_length=50, required=True)
+    month_birth = serializers.ChoiceField(choices=MonthBirth.choices, default=MonthBirth.choices[0][0])
+    team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all())
+    created_at_month = serializers.IntegerField()
+    created_at = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Person.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.month_birth = validated_data.get('month_birth', instance.month_birth)
+        instance.team = validated_data.get('team', instance.team)
+        instance.created_at_month = validated_data.get('created_at_month', instance.created_at_month)
+        instance.created_at = validated_data.get('created_at', instance.created_at)
+        instance.save()
+        return instance
+
+    def validate_name(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("Nazwa nie powinna zawierać wartości liczbowych!")
+        return value
+
+    def created_at_month_validation(self, data):
+        if data['created_at_month'] > datetime.datetime.today().month:
+            raise serializers.ValidationError("Dane w polu 'miesiac_dodania' są nieprawidłowe!")
+        return data
